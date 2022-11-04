af2d41cdd14145bcc4c683d0d15762a0ccddc17d
Dre <dredawkins11@gmail.com>
2022-11-03 22:42:46 -0500

    fixed nav menu

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 let nav = document.getElementById("navmenu");
+let links = document.getElementsByClassName("nav-item");
 
 nav.addEventListener("click", toggleHamburger);
-nav.addEventListener("mouseleave", toggleHamburger);
-nav.addEventListener("animationstart", () => {
+nav.addEventListener("mouseleave", closeHamburger);
+nav.addEventListener("animationstart", () => {});
 
-})
-
-let animationRunning = false
+let animationRunning = false;
 
 function trackAnimation(e) {
-    animationRunning = true
-    
+    animationRunning = true;
 }
 
 function toggleHamburger() {
-
     /*
-    *  Check if animation currently running 
-    *  if so listen for animation end and call this again
-    *
-    *  If not start next animation
-    *
-    */
-    while (animationRunning) {
-        
-    }
+     *  Check if animation currently running
+     *  if so listen for animation end and call this again
+     *
+     *  If not start next animation
+     *
+     */
 
     nav.style.animation = "none";
     nav.offsetHeight;
@@ -33,9 +27,34 @@ function toggleHamburger() {
     if (nav.classList.contains("opened")) {
         nav.classList.remove("opened");
         nav.classList.add("closed");
+        links.forEach((el) => {
+            el.classList.remove("opened");
+        });
+
         return;
     }
 
     nav.classList.remove("closed");
     nav.classList.add("opened");
+    links.forEach((el) => {
+        el.classList.add("opened");
+    });
+}
+
+function closeHamburger() {
+    
+    if (!nav.classList.contains("opened") || nav.classList.contains("closed")) {
+        return;
+    }
+    
+    nav.style.animation = "none";
+    nav.offsetHeight;
+    nav.style.animation = null;
+    
+    nav.classList.remove("opened");
+    nav.classList.add("closed");
+
+    links.forEach((el) => {
+        el.classList.add("opened");
+    });
 }
